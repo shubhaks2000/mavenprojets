726711b76d104ebd9ba07d75aba67bca560cfe29
shubhaks2000 <shubaks2000@gmail.com>
2023-06-16 12:21:03 +0530

    new files added

---
+++ src/test/java/com/shu/AppTest.java
@@ -0,0 +1,23 @@
+package com.shu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.shubha.App;
+public class AppTest {
+	@Test
+	public void testlogin1()
+	{
+	  App myapp=new App();
+      Assert.assertEquals(0,myapp.userlogin("Admin","admin123"));
+	}
+	
+	@Test
+	public void testlogin2()
+	{
+		
+	  App myapp=new App();
+      Assert.assertEquals(1,myapp.userlogin("Admin","admin123"));
+	}
+
+
+}
